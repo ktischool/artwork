7076a097db0c17e9c1773146ae66380aff944326
ktischool <148981478+ktischool@users.noreply.github.com>
2023-10-26 21:37:34 -0600

    Update run.php

--- run.php
+++ run.php
@@ -1,37 +1,37 @@
-<?php 
-$serve="localhost";
+<?php
+ $serve="localhost";
 $username="root";
 $password="";
 $dbname="artist";
 
 $conn=mysqli_connect($serve,$username,$password,$dbname);
 
-if (isset($_POST['send'])) {
-if(!empty($_POST['fname']) && !empty($_POST['email']) &&!empty($_POST['phone']) &&!empty($_POST['messag'])){
-
- $fname=$_POST['fname'];
+if(isset($_POST['send'])){
+ if(!empty($_POST['fname'])&& !empty($_POST['email'])&&!empty($_POST['phone'])&& !empty($_POST['messag'])) {
+  
+$fname=$_POST['fname'];
  $email=$_POST['email'];
- $phone=$_POST['phone'];
- $messag=$_POST['messag'];
- 
+  $phone=$_POST['phone'];
+  $messag=$_POST['messag']; 
+  
  $query="INSERT INTO artist_tb(fname,email,phone,messag) values('$fname','$email','$phone','$messag')";
  $run= mysqli_query($conn,$query) or die (mysqli_error());
-
- if($run){
+  
+if($run){
     echo "REGISTER SUCESSFULLY";
 }
-
 else{
     echo "NOT REGISTER SUCESSFULLY";
 }
-}
-
+ } 
 else{
     echo "All fields reqiured";
 }
 }
+
 ?>
-<br><br>
+ <br><br>
     <button>
    <a href="index.html">Back</a>
-    </button>
+    </button>      
+
